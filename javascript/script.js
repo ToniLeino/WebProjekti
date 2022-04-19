4b808d20ba05cce2b70b15ae9c50e6b7f0f2261a
LeeviHe <l.helttunen@gmail.com>
2022-04-19 19:55:07 +0300

    scriptiin viimeiset käännökset

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-let pisteet = 0;
+let points = 0;
 const RIGHT_ANSWER = "Vastauksesi on oikein! Hyvää työtä!";
 const WRONG_ANSWER = "Vastauksesi on väärin!";
 const ANSWER_ERROR = "Valitse vastaus ennen tarkistusta!";
@@ -9,7 +9,6 @@ function question1() {
     let secondAnswer = document.getElementById("select1.2");
     let thirdAnswer = document.getElementById("select1.3");
     let fourthAnswer = document.getElementById("select1.4");
-    let radio = document.querySelector("input[type=radio]:checked");
     /*tämä on vain lintuvisan oikea vastaus, täytyy keksiä joku muu tapa tähän */
     let correctAnswer = document.getElementById("select1.3").checked;
     result.innerHTML = "";
@@ -21,7 +20,7 @@ function question1() {
     ) {
         if (correctAnswer) {
             result.innerHTML = RIGHT_ANSWER;
-            pisteet++;
+            points++;
         } else {
             result.innerHTML = WRONG_ANSWER;
         }
@@ -40,8 +39,7 @@ function question2() {
     let secondAnswer = document.getElementById("select2.2");
     let thirdAnswer = document.getElementById("select2.3");
     let fourthAnswer = document.getElementById("select2.4");
-    let radio = document.querySelector("input[type=radio]:checked");
-    let oikeaVastaus = document.getElementById("select2.1").checked;
+    let correctAnswer = document.getElementById("select2.1").checked;
     result.innerHTML = "";
     if (
         firstAnswer.checked ||
@@ -49,9 +47,9 @@ function question2() {
         thirdAnswer.checked ||
         fourthAnswer.checked
     ) {
-        if (oikeaVastaus) {
+        if (correctAnswer) {
             result.innerHTML = RIGHT_ANSWER;
-            pisteet++;
+            points++;
         } else {
             result.innerHTML = WRONG_ANSWER;
         }
@@ -70,8 +68,7 @@ function question3() {
     let secondAnswer = document.getElementById("select3.2");
     let thirdAnswer = document.getElementById("select3.3");
     let fourthAnswer = document.getElementById("select3.4");
-    let radio = document.querySelector("input[type=radio]:checked");
-    let oikeaVastaus = document.getElementById("select3.4").checked;
+    let correctAnswer = document.getElementById("select3.4").checked;
     result.innerHTML = "";
     if (
         firstAnswer.checked ||
@@ -79,9 +76,9 @@ function question3() {
         thirdAnswer.checked ||
         fourthAnswer.checked
     ) {
-        if (oikeaVastaus) {
+        if (correctAnswer) {
             result.innerHTML = RIGHT_ANSWER;
-            pisteet++;
+            points++;
         } else {
             result.innerHTML = WRONG_ANSWER;
         }
@@ -100,8 +97,7 @@ function question4() {
     let secondAnswer = document.getElementById("select4.2");
     let thirdAnswer = document.getElementById("select4.3");
     let fourthAnswer = document.getElementById("select4.4");
-    let radio = document.querySelector("input[type=radio]:checked");
-    let oikeaVastaus = document.getElementById("select4.2").checked;
+    let correctAnswer = document.getElementById("select4.2").checked;
     result.innerHTML = "";
     if (
         firstAnswer.checked ||
@@ -109,9 +105,9 @@ function question4() {
         thirdAnswer.checked ||
         fourthAnswer.checked
     ) {
-        if (oikeaVastaus) {
+        if (correctAnswer) {
             result.innerHTML = RIGHT_ANSWER;
-            pisteet++;
+            points++;
         } else {
             result.innerHTML = WRONG_ANSWER;
         }
@@ -130,8 +126,7 @@ function question5() {
     let secondAnswer = document.getElementById("select5.2");
     let thirdAnswer = document.getElementById("select5.3");
     let fourthAnswer = document.getElementById("select5.4");
-    let radio = document.querySelector("input[type=radio]:checked");
-    let oikeaVastaus = document.getElementById("select5.4").checked;
+    let correctAnswer = document.getElementById("select5.4").checked;
     result.innerHTML = "";
     if (
         firstAnswer.checked ||
@@ -139,9 +134,9 @@ function question5() {
         thirdAnswer.checked ||
         fourthAnswer.checked
     ) {
-        if (oikeaVastaus) {
+        if (correctAnswer) {
             result.innerHTML = RIGHT_ANSWER;
-            pisteet++;
+            points++;
         } else {
             result.innerHTML = WRONG_ANSWER;
         }
@@ -155,6 +150,6 @@ function question5() {
 }
 
 function checkResult() {
-    let loppuTulos = pisteet + " / 5 !";
-    document.getElementById("lopputulos").innerHTML = loppuTulos;
+    let finalResult = points + " / 5 !";
+    document.getElementById("printFinalResult").innerHTML = finalResult;
 }

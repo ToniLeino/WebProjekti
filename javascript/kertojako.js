5ea021f1504dcfa21b1bddc07b41590c18e99608
Riku Björn <riku.bjorn@gmail.com>
2022-04-21 14:58:01 +0300

    tehtävät toiminnassa

--- javascript/kertojako.js
+++ javascript/kertojako.js
@@ -1,81 +1,16 @@
-//random generaattori
 let points = 0;
 const RIGHT_ANSWER = "Vastauksesi on oikein! Hyvää työtä!";
 const WRONG_ANSWER = "Vastauksesi on väärin!";
 const ANSWER_ERROR = "Valitse vastaus ennen tarkistusta!";
 
-//1.kysymys
-
-/*let min = 1;
-let max = 10;
-let int1 = Math.floor(Math.random() * (max - min + 1)) + min;
-let int2 = Math.floor(Math.random() * (max - min + 1)) + min;
-
-document.getElementById("kysymysYksi").innerHTML = int1 + " " + "*" + " " + int2 + " = ";
-
-let kertoAnswer = int1 * int2;
-
-
-
-//2.kysymys
-
-let min2 = 100;
-let max2 = 2;
-let int3 = Math.floor(Math.random() * (max2 - min2 + 1)) + min2;
-let int4 = Math.floor(Math.random() * (max2 - min2 + 1)) + min2;
-
-document.getElementById("kysymysKaksi").innerHTML = int3 + " " + "/" + " " + int4 + " = ";
-
-let jakoAnswer = int3 / int4;
-
-function question22() {
-    let vastauskentta22 = document.getElementById("vastaus12").value;
-        if (vastauskentta22 == jakoAnswer) {
-            tarkistus22.innerHTML = RIGHT_ANSWER;
-            points++;
-        } else {
-            tarkistus22.innerHTML = WRONG_ANSWER;   
-        }
-        document.getElementById("check-btn1").disabled = true;
-    
-    for(let i = 0; i > jakoAnswer; ){}
-
-}*/
-
-//random generaattori
-
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-
-
-
-
 //Matematiikan tehtävä vaihtoehtoja arrayssä
-let kerto1 = ["3 * 5 = ", "6 * 7 = ", "9 * 4 = ", "6 * 6 = "];
-let kerto2 = ["6 * 3 * 2 = ", "3 * 3 * 3 = ", "3 * 4 * 3 = ", "7 * 2 * 2 = "];
+let kerto1 = ["3 * 5 = ", "6 * 7 = ", "9 * 4 = ", "8 * 6 = "];
+let kerto2 = ["6 * 3 * 2 = ", "3 * 3 * 3 = ", "3 * 4 * 4 = ", "7 * 2 * 2 = "];
 let jako1 = ["64 / 8 = ", "81 / 9 = ", "36 / 6 = ", "50 / 5 = "];
 let jako2 = ["45 / 5 / 3 = ", "72 / 9 / 4 = ", "100 / 10 / 5 = ", "27 / 9 / 3 = "];
 let kertoJako = ["4 * 7 / 2 = ", "56 / 7 * 9 = ", "8 * 6 / 4 = ", "36 / 6 * 5 = "];
 
-/*//matematiikan tehtävien oikeat vaihtoehdot
-let kerto1Oikea = ["15", "42", "36", "36"];
-let kerto2Oikea = ["36", "27", "36", "28"];
-let jako1Oikea = ["8", "9", "6", "10"];
-let jako2Oikea = ["3", "2", "2", "1"];
-let kertoJakoOikea = ["14", "64", "12", "30"]*/
-
-
-
-//matematiikan tehtävien väärät vaihtoehdot
-let kerto1Vaara = ["68", "54", "13", "17", "16", "20", "74", "60"];
-let kerto2Vaara = ["36", "27", "33", "28", "74", "55", "6", "73"];
-let jako1Vaara = ["8", "9", "6", "10", "13", "19", "32", "30"];
-let jako2Vaara = ["3", "4", "88", "90", "5", "8", "10", "13"];
-let kertoJakoVaara = ["14", "64", "12", "30", "13", "16", "21", "53"]
-
-let vastaus = 15
+//Tehtävien tulostus arraystä
 
 let questionYksi = kerto1[(Math.random() * kerto1.length) | 0]
 document.getElementById("kysymysYksi").innerHTML = questionYksi
@@ -88,24 +23,134 @@ document.getElementById("kysymysNelja").innerHTML = questionNelja
 let questionViisi = kertoJako[(Math.random() * kertoJako.length) | 0]
 document.getElementById("kysymysViisi").innerHTML = questionViisi
 
-/*//1.x
-
-let vastaus11 = kerto1Vaara[(Math.random() * kerto1Vaara.length) | 0]
-kerto1Vaara = kerto1Vaara.filter(function(item) {
-    return item !== vastaus11
-})
-document.getElementById("vastaus1.1").innerHTML = vastaus11*/
-
+//tehtävä 1
 
 function question11() {
     let vastauskentta = document.getElementById("vastaus11").value;
-        if (vastauskentta == 15 && questionYksi == "3 * 5 = ") {
+        if (questionYksi == "3 * 5 = " && vastauskentta == 15) {
             tarkistus11.innerHTML = RIGHT_ANSWER;
             points++;
-        } else {
+        } 
+        else if (questionYksi == "6 * 7 = " && vastauskentta == 42){
+            tarkistus11.innerHTML = RIGHT_ANSWER;
+            points++;
+        }
+        else if (questionYksi == "9 * 4 = " && vastauskentta == 36){
+            tarkistus11.innerHTML = RIGHT_ANSWER;
+            points++;
+       }
+       else if (questionYksi == "8 * 6 = " && vastauskentta == 48){
+            tarkistus11.innerHTML = RIGHT_ANSWER;
+            points++;
+        }
+        else {
             tarkistus11.innerHTML = WRONG_ANSWER;   
         }
-        document.getElementById("check-btn").disabled = true;
+        document.getElementById("check-btn11").disabled = true;
+}
+
+//tehtävä 2
+
+function question22() {
+    let vastauskentta2 = document.getElementById("vastaus12").value;
+        if (questionKaksi == "6 * 3 * 2 = " && vastauskentta2 == 36) {
+            tarkistus22.innerHTML = RIGHT_ANSWER;
+            points++;
+        } 
+        else if (questionKaksi == "3 * 3 * 3 = " && vastauskentta2 == 27){
+            tarkistus22.innerHTML = RIGHT_ANSWER;
+            points++;
+        }
+        else if (questionKaksi == "3 * 4 * 4 = " && vastauskentta2 == 48){
+            tarkistus22.innerHTML = RIGHT_ANSWER;
+            points++;
+       }
+       else if (questionKaksi == "7 * 2 * 2 = " && vastauskentta2 == 28){
+            tarkistus22.innerHTML = RIGHT_ANSWER;
+            points++;
+        }
+        else {
+            tarkistus22.innerHTML = WRONG_ANSWER;   
+        }
+        document.getElementById("check-btn12").disabled = true;
+}
+
+//tehtävä 3
+
+function question33() {
+    let vastauskentta3 = document.getElementById("vastaus13").value;
+        if (questionKolme == "64 / 8 = " && vastauskentta3 == 8) {
+            tarkistus33.innerHTML = RIGHT_ANSWER;
+            points++;
+        } 
+        else if (questionKolme == "81 / 9 = " && vastauskentta3 == 9){
+            tarkistus33.innerHTML = RIGHT_ANSWER;
+            points++;
+        }
+        else if (questionKolme == "36 / 6 = " && vastauskentta3 == 6){
+            tarkistus33.innerHTML = RIGHT_ANSWER;
+            points++;
+       }
+       else if (questionKolme == "50 / 5 = " && vastauskentta3 == 10){
+            tarkistus33.innerHTML = RIGHT_ANSWER;
+            points++;
+        }
+        else {
+            tarkistus33.innerHTML = WRONG_ANSWER;   
+        }
+        document.getElementById("check-btn13").disabled = true;
+}
+
+//tehtävä 4
+
+function question44() {
+    let vastauskentta4 = document.getElementById("vastaus14").value;
+        if (questionNelja == "45 / 5 / 3 = " && vastauskentta4 == 3) {
+            tarkistus44.innerHTML = RIGHT_ANSWER;
+            points++;
+        } 
+        else if (questionNelja == "72 / 9 / 4 = " && vastauskentta4 == 2){
+            tarkistus44.innerHTML = RIGHT_ANSWER;
+            points++;
+        }
+        else if (questionNelja == "100 / 10 / 5 = " && vastauskentta4 == 2){
+            tarkistus44.innerHTML = RIGHT_ANSWER;
+            points++;
+       }
+       else if (questionNelja == "27 / 9 / 3 = " && vastauskentta4 == 1){
+            tarkistus44.innerHTML = RIGHT_ANSWER;
+            points++;
+        }
+        else {
+            tarkistus44.innerHTML = WRONG_ANSWER;   
+        }
+        document.getElementById("check-btn14").disabled = true;
+}
+
+//tehtävä 5
+
+function question55() {
+    let vastauskentta5 = document.getElementById("vastaus15").value;
+        if (questionViisi == "4 * 7 / 2 = " && vastauskentta5 == 14) {
+            tarkistus55.innerHTML = RIGHT_ANSWER;
+            points++;
+        } 
+        else if (questionViisi == "56 / 7 * 9 = " && vastauskentta5 == 64){
+            tarkistus55.innerHTML = RIGHT_ANSWER;
+            points++;
+        }
+        else if (questionViisi == "8 * 6 / 4 = " && vastauskentta5 == 12){
+            tarkistus55.innerHTML = RIGHT_ANSWER;
+            points++;
+       }
+       else if (questionViisi == "36 / 6 * 5 = " && vastauskentta5 == 30){
+            tarkistus55.innerHTML = RIGHT_ANSWER;
+            points++;
+        }
+        else {
+            tarkistus55.innerHTML = WRONG_ANSWER;   
+        }
+        document.getElementById("check-btn15").disabled = true;
 }
 
 

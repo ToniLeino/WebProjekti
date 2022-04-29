b0ebfda41a9e30a63fa2cfdc1d6bd6ea94e06022
EetuHuttula <100779993+EetuHuttula@users.noreply.github.com>
2022-04-29 17:02:58 +0300

    Confetit lisätty

--- javascript/mathplus.js
+++ javascript/mathplus.js
@@ -114,6 +114,26 @@
         }
         else{
             document.getElementById("printFinalResult").innerHTML = finalResult + "<br>" + pisteet3_5
+            
+            // Seuraava koodi on otettu netistä lähde: https://dev.to/official_fire/creating-a-confetti-effect-in-5-minutes-16h3
+            // for starting the confetti
+            const start = () => {
+                setTimeout(function() {
+                    confetti.start();
+                }, 1000);
+                // 1000 is time that after 1 second start the confetti ( 1000 = 1 sec)
+            };
+
+            //  for stopping the confetti
+
+            const stop = () => {
+                setTimeout(function() {
+                    confetti.stop();
+                }, 5000); // 5000 is time that after 5 second stop the confetti ( 5000 = 5 sec)
+            };
+            // after this here we are calling both the function so it works
+            start();
+            stop();
         }
     }
     else {

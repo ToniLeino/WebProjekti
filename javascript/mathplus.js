11ba794fccd59a7a225c1bdfa71ac901564f21e6
EetuHuttula <100779993+EetuHuttula@users.noreply.github.com>
2022-05-03 15:22:17 +0300

    Header muutoksia.

--- javascript/mathplus.js
+++ javascript/mathplus.js
@@ -141,13 +141,3 @@
             "Vastaa ensin kaikkiin kysymyksiin!";
     }
     }
-
-
-
-    //header toggle buttoni
-    const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
